e65a240e9a6dca69e33a90ed077e23bc8ef68ced
Yasin <271155397@qq.com>
2020-02-20 23:10:27 +0800

    add eureka server and test platform

---
+++ backend-testplatform/backend-test-service-consumer/src/main/java/com/yasin/meetingfilm/backend/testserviceconsumer/BackendTestServiceConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.yasin.meetingfilm.backend.testserviceconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTestServiceConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendTestServiceConsumerApplication.class, args);
+    }
+
+}
---
+++ backend-testplatform/backend-test-service-provider/src/main/java/com/yasin/meetingfilm/backend/testserviceprovider/BackendTestServiceProviderApplication.java
@@ -0,0 +1,13 @@
+package com.yasin.meetingfilm.backend.testserviceprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTestServiceProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendTestServiceProviderApplication.class, args);
+    }
+
+}
